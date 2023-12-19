1bb3bd63c47bb429289fc5264896267c81be8fa9
DenZaiyy <denzaiyy@outlook.fr>
2023-12-19 19:23:15 +0100

    feat: :arrow_up: add toast, redux + :wrench: files, persist, and stripe dependencies

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,19 @@
 
 import {SessionProvider} from "next-auth/react";
 import React from "react";
+import {Provider} from "react-redux";
+import {persistor, store} from "@/redux/store";
+import {PersistGate} from "redux-persist/integration/react";
 
 const Layout = ({children}:{children: React.ReactNode}) => {
 	return (
-		<SessionProvider>
-			{children}
-		</SessionProvider>
+		<Provider store={store}>
+			<PersistGate persistor={persistor} loading={null}>
+				<SessionProvider>
+					{children}
+				</SessionProvider>
+			</PersistGate>
+		</Provider>
 	);
 };
 
