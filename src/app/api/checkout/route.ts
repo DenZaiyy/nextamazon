8928b810590200720b847f46bf75ae9b71a4a2ee
DenZaiyy <denzaiyy@outlook.fr>
2023-12-19 19:57:42 +0100

    feat: :hammer: add checkout stripe payment

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,44 @@
+import {NextRequest, NextResponse} from "next/server";
+import Stripe from "stripe";
+import {ProductType} from "../../../../type";
+
+export const POST = async (req: NextRequest) => {
+	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+	try {
+		const reqBody = await req.json()
+		const {items, email} = await reqBody
+
+		const extractingItems = await items.map((item: ProductType) => ({
+			quantity: item.quantity,
+			price_data: {
+				currency: "usd",
+				unit_amount: item.price * 100,
+				product_data: {
+					name: item.title,
+					description: item.description,
+					images: [item.image],
+				},
+			}
+		}))
+
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ['card'],
+			line_items: extractingItems,
+			mode: 'payment',
+			success_url: `${process.env.NEXTAUTH_URL}/success`,
+			cancel_url: `${process.env.NEXTAUTH_URL}/cancel`,
+			metadata: {
+				email
+			}
+		})
+
+		return NextResponse.json({
+			message: "Connection is Active",
+			success: true,
+			id: session.id,
+		})
+	} catch (e: any) {
+		return NextResponse.json({error: e?.message}, {status: 500})
+	}
+}
